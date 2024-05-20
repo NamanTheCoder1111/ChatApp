ba88baadab761ddf924c8c6f51ef2876c20c2a75
Naman Jain <108236312+NamanTheCoder1111@users.noreply.github.com>
2024-05-20 16:43:39 +0530

    with lodce

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server,{
     cors:{
-        origin: 'https://naman-chatapp.netlify.app/' ||'http://localhost:5173',
+        origin: 'https://naman-chatapp.netlify.app/users/sign_in' ||'http://localhost:5173',
     }
 });
 require("dotenv").config();
